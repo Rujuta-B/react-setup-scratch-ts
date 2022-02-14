5310edbf392375bfcf52b7896f21929557c9fecb
brujuta <97433576+Rujuta-B@users.noreply.github.com>
2022-02-14 22:17:05 +0530

    react-setup-from-scratch-complete

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import "./App.css";
+
+ReactDOM.render( 
+  <div> 
+    <h3>Hello World</h3>
+    <h4>This is a React setup in Typescript from scratch without using CRA along with flexible toolchain like Babel and Webpack</h4>
+
+
+
+  </div>, 
+  document.getElementById('root')
+);
